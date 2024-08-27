1c4f0ac9b7a08b4f05deedc6c26f35574706377c
ChristianHopf <cehopf@gmail.com>
2024-08-27 14:28:35 -0400

    Removed unused import

--- app/components/header.ts
+++ app/components/header.ts
@@ -2,7 +2,6 @@ import Component from '@glimmer/component';
 import { service } from '@ember/service';
 import StyleService from 'souls-tracker/services/style';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 
 export default class Header extends Component {
   @service declare style: StyleService;

5a59b577233c28424474aa9c503aba72008cb3f5
ChristianHopf <cehopf@gmail.com>
2024-08-27 14:03:47 -0400

    Header can toggle between light and dark mode

--- app/components/header.ts
+++ app/components/header.ts
@@ -1,3 +1,14 @@
 import Component from '@glimmer/component';
+import { service } from '@ember/service';
+import StyleService from 'souls-tracker/services/style';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default class Header extends Component {}
+export default class Header extends Component {
+  @service declare style: StyleService;
+
+  @action
+  changeMode() {
+    this.style.toggleMode();
+  }
+}

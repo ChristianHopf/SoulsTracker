c667fa885ef7d930a64fb62c3a03d45bf8dba9c1
ChristianHopf <cehopf@gmail.com>
2024-08-30 11:28:07 -0400

    InputRow, UserInfo, StatsContainer tests w MockUserService

--- tests/integration/components/input-row-test.js
+++ tests/integration/components/input-row-test.js
@@ -1,26 +1,27 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'souls-tracker/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { find, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import Service from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+
+class MockUserService extends Service {
+  @tracked ownedGames = [{ name: 'DARK SOULS™: REMASTERED', appid: '570940' }];
+}
 
 module('Integration | Component | input-row', function (hooks) {
   setupRenderingTest(hooks);
+  hooks.beforeEach(function () {
+    this.owner.register('service:user', MockUserService);
+  });
 
-  test('it renders', async function (assert) {
+  test('it renders the FindUser and GameSelect components', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
     await render(hbs`<InputRow />`);
 
-    assert.dom().hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <InputRow>
-        template block text
-      </InputRow>
-    `);
-
-    assert.dom().hasText('template block text');
+    assert.ok(find('[data-test-find-user]'));
+    assert.ok(find('[data-test-game-select]'));
   });
 });
--- tests/integration/components/stats/container-test.js
+++ tests/integration/components/stats/container-test.js
@@ -2,9 +2,22 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'souls-tracker/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import Service from '@ember/service';
+
+class MockUserService extends Service {
+  userInfo = {
+    steamid: '76561198099631791',
+    personaname: 'xman720',
+    avatarmedium:
+      'https://avatars.steamstatic.com/5af6623ce2a9d41363a12605d9d015febafa30ad_medium.jpg',
+  };
+}
 
 module('Integration | Component | stats/container', function (hooks) {
   setupRenderingTest(hooks);
+  hooks.beforeEach(function () {
+    this.owner.register('service:user', MockUserService);
+  });
 
   test('it renders UserInfo and stats components', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
@@ -13,5 +26,8 @@ module('Integration | Component | stats/container', function (hooks) {
     await render(hbs`<Stats::Container />`);
 
     assert.dom('[data-test-user-info]').exists();
+    assert.dom('[data-test-playtime]').exists();
+    assert.dom('[data-test-bosses]').exists();
+    assert.dom('[data-test-achievements]').exists();
   });
 });

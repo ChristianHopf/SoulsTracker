59b405d1b9e5001780a793304a0ac9eee2f000e4
ChristianHopf <cehopf@gmail.com>
2024-08-30 15:59:38 -0400

    UserInfo no longer needs to register UserService

--- app/components/user-info.ts
+++ app/components/user-info.ts
@@ -1,7 +1,3 @@
 import Component from '@glimmer/component';
-import { service } from '@ember/service';
-import type UserService from 'souls-tracker/services/user';
 
-export default class UserInfo extends Component {
-  @service declare user: UserService;
-}
+export default class UserInfo extends Component {}

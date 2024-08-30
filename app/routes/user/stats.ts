167ad3dc32de5bbdf5587e68010a294d42c81942
ChristianHopf <cehopf@gmail.com>
2024-08-30 17:11:46 -0400

    UserStatsRoute registers the Stats service

--- app/routes/user/stats.ts
+++ app/routes/user/stats.ts
@@ -1,6 +1,10 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+import type StatsService from 'souls-tracker/services/stats';
 
 export default class UserStatsRoute extends Route {
+  @service declare stats: StatsService;
+
   async model(params: { game: string }) {
     console.log(params.game);
     return params.game;

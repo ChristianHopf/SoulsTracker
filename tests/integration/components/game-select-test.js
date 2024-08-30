4e3b84d8344384207018e66c0865a89ae565b866
ChristianHopf <cehopf@gmail.com>
2024-08-30 11:27:34 -0400

    GameSelect test with MockUserService

--- tests/integration/components/game-select-test.js
+++ tests/integration/components/game-select-test.js
@@ -2,9 +2,17 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'souls-tracker/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import Service from '@ember/service';
+
+class MockUserService extends Service {
+  ownedGames = [{ name: 'DARK SOULS™: REMASTERED', appid: '570940' }];
+}
 
 module('Integration | Component | game-select', function (hooks) {
   setupRenderingTest(hooks);
+  hooks.beforeEach(function () {
+    this.owner.register('service:user', MockUserService);
+  });
 
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
@@ -12,15 +20,6 @@ module('Integration | Component | game-select', function (hooks) {
 
     await render(hbs`<GameSelect />`);
 
-    assert.dom().hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <GameSelect>
-        template block text
-      </GameSelect>
-    `);
-
-    assert.dom().hasText('template block text');
+    assert.dom('span').containsText('Select Game');
   });
 });

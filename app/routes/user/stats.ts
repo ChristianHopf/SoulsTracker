cab7dcae981558704cb1375164f9dcf3e2677f03
ChristianHopf <cehopf@gmail.com>
2024-09-01 16:18:46 -0400

    Stats service fetches playtime for user.stats route

--- app/routes/user/stats.ts
+++ app/routes/user/stats.ts
@@ -1,12 +1,27 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import type StatsService from 'souls-tracker/services/stats';
+import type UserService from 'souls-tracker/services/user';
 
 export default class UserStatsRoute extends Route {
+  @service declare user: UserService;
   @service declare stats: StatsService;
 
-  async model(params: { game: string }) {
-    console.log(params.game);
-    return params.game;
+  async model(params: { gameid: string }) {
+    console.log(params);
+    // use game id to get appid
+    const selectedGame = this.user.ownedGames?.find(
+      (game) => game.id === params.gameid,
+    );
+    const steamid = this.user.userInfo?.steamid;
+    if (steamid && selectedGame) {
+      const playtime = await this.stats.fetchPlaytime(
+        steamid,
+        selectedGame?.appid,
+      );
+      console.log(playtime);
+      return playtime;
+    }
+    return null;
   }
 }
--- app/services/stats.ts
+++ app/services/stats.ts
@@ -2,8 +2,8 @@ import Service from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 export interface Playtime {
-  playtime_forever: number;
-  playtime_2weeks: number;
+  playtime_forever: string;
+  playtime_2weeks: string;
 }
 
 export interface Bosses {
@@ -15,7 +15,7 @@ export interface Achievement {
   id: string;
   name: string;
   description: string;
-  unlocktime: number;
+  unlocktime: string;
   icon: string;
 }
 
@@ -23,6 +23,28 @@ export default class StatsService extends Service {
   @tracked playtime: Playtime | null = null;
   @tracked bosses: Bosses | null = null;
   @tracked achievements: Achievement[] | null = null;
+
+  async fetchPlaytime(
+    steamid: string,
+    appid: string,
+  ): Promise<Playtime | null> {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/api/playtime/${steamid}/${appid}`,
+      );
+      if (!response.ok) {
+        console.error(`Error: status ${response.status}`);
+        this.playtime = null;
+        return null;
+      }
+      const responseJson = await response.json();
+      this.playtime = responseJson.data;
+      return responseJson.data;
+    } catch (err) {
+      console.error('Failed to fetch playtime', err);
+      return null;
+    }
+  }
 }
 
 // Don't remove this declaration: this is what enables TypeScript to resolve

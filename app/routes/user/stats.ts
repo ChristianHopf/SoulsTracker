56224ea60268f2dfe2373ac0ae39cf92f097691d
ChristianHopf <cehopf@gmail.com>
2024-09-03 17:28:15 -0400

    UserStats route handles unsupported games

--- app/routes/user/stats.ts
+++ app/routes/user/stats.ts
@@ -10,11 +10,18 @@ export default class UserStatsRoute extends Route {
   async model(params: { gameid: string }) {
     // console.log(params);
     // use game id to get appid
+    const steamid = this.user.userInfo?.steamid;
     const selectedGame = this.user.ownedGames?.find(
       (game) => game.id === params.gameid,
     );
-    const steamid = this.user.userInfo?.steamid;
-
+    console.log(selectedGame?.appid);
+    // Return null for games with support yet to be implemented
+    switch (selectedGame?.appid.toString()) {
+      case '570940':
+        break;
+      default:
+        return null;
+    }
     if (steamid && selectedGame) {
       const stats = this.stats.fetchStats(steamid, selectedGame.appid);
       return stats;

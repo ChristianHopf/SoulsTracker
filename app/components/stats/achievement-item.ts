9f2167dc52873311f9dd7b928127b8b4038a9795
ChristianHopf <cehopf@gmail.com>
2024-10-06 15:31:24 -0400

    AchievementItem gets date string from unlocktime

--- app/components/stats/achievement-item.ts
+++ app/components/stats/achievement-item.ts
@@ -1,3 +1,12 @@
 import Component from '@glimmer/component';
+// import type { Achievement } from 'souls-tracker/services/stats';
 
-export default class StatsAchievementItem extends Component {}
+export default class StatsAchievementItem extends Component {
+  get unlockTimeDateString(): string {
+    // Still don't understand this error, but it works
+    const unlocktime = parseInt(this.args.achievement.unlocktime);
+    console.log(unlocktime);
+    const date = new Date(unlocktime * 1000).toLocaleDateString();
+    return date;
+  }
+}

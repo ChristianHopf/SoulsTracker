7e01e5f59e06092b9c2fe197ddb8026f2d008260
ChristianHopf <cehopf@gmail.com>
2024-10-06 15:49:13 -0400

    Active sorting mode is semibold

--- app/components/stats/achievement-sort.ts
+++ app/components/stats/achievement-sort.ts
@@ -9,6 +9,21 @@ export default class StatsAchievementSort extends Component {
   @service declare stats: StatsService;
   @tracked hidden: boolean = true;
 
+  // Really don't like this, there should be an elegant solution with a radiogroup or something.
+  // But Ember doesn't allow 'a === b' expressions in template conditionals
+  get activeDateNewSort() {
+    return this.stats.achievementSort === 'date-new';
+  }
+  get activeDateOldSort() {
+    return this.stats.achievementSort === 'date-old';
+  }
+  get activeRarityMostSort() {
+    return this.stats.achievementSort === 'rarity-most';
+  }
+  get activeRarityLeastSort() {
+    return this.stats.achievementSort === 'rarity-least';
+  }
+
   @action
   toggleSortDropdown() {
     this.hidden = !this.hidden;

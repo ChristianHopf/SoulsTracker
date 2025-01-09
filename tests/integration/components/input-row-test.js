d7b283e9d32c0316f6336b3a7014072e04b8f984
ChristianHopf <cehopf@gmail.com>
2025-01-08 19:16:56 -0500

    Unnecessary mock service

--- tests/integration/components/input-row-test.js
+++ tests/integration/components/input-row-test.js
@@ -2,18 +2,27 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'souls-tracker/tests/helpers';
 import { find, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
-import Service from '@ember/service';
-import { tracked } from '@glimmer/tracking';
+// import Service from '@ember/service';
+// import { tracked } from '@glimmer/tracking';
 
-class MockUserService extends Service {
-  @tracked ownedGames = [{ name: 'DARK SOULS™: REMASTERED', appid: '570940' }];
-}
+// Unnecessary for this test, but I'll leave it here
+// class MockUserService extends Service {
+// @tracked ownedGames = [
+//   {
+//     name: 'DARK SOULS™: REMASTERED',
+//     id: 'dark-souls-remastered',
+//     appid: '570940',
+//   },
+// ];
+// }
 
 module('Integration | Component | input-row', function (hooks) {
   setupRenderingTest(hooks);
-  hooks.beforeEach(function () {
-    this.owner.register('service:user', MockUserService);
-  });
+
+  // Unnecessary for this test, but I'll leave it here
+  // hooks.beforeEach(function () {
+  //   this.owner.register('service:user', MockUserService);
+  // });
 
   test('it renders the FindUser and GameSelect components', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
